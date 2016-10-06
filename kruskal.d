bc4306f707b02fe62f6e841dd691f1103028dffd
sakurakawaiiyo <logic.mcwu@gmail.com>
2016-10-06 16:09:36 -0400

    Revert "added lemmas from ramsey"
    This reverts commit 87d5d23cd8199ae5a4b90fd759a85aab42c551a1.

--- kruskal.d
+++
@@ -1,4 +0,0 @@
-\Users\minch\OneDrive\Documents\GitHub\Kruskal.lean\kruskal.olean: \
- \Lean\lean-0.2.0-windows\lib\lean\library\data\fin.olean \
- \Lean\lean-0.2.0-windows\lib\lean\library\data\list\default.olean \
- \Lean\lean-0.2.0-windows\lib\lean\library\init\default.olean
--- theorem.d
+++
@@ -1,6 +0,0 @@
-\Users\minch\OneDrive\Documents\GitHub\Kruskal.lean\theorem.olean: \
- \Users\minch\OneDrive\Documents\GitHub\Kruskal.lean\kruskal.olean \
- \Users\minch\OneDrive\Documents\GitHub\Kruskal.lean\lemmas1.olean \
- \Users\minch\OneDrive\Documents\GitHub\Kruskal.lean\lemmas2.olean \
- \Lean\lean-0.2.0-windows\lib\lean\library\data\list\default.olean \
- \Lean\lean-0.2.0-windows\lib\lean\library\init\default.olean
